c38863a8835ef9416cb6e6a45d81e9d58205e82b
Sandeater88 <fw.ipowo@gmail.com>
2024-06-10 18:14:39 +0200

    whyyyyyyyyyyyy

--- itssoover/Assets/mystuff/scripts/player/Ammo.cs
+++ itssoover/Assets/mystuff/scripts/player/Ammo.cs
@@ -1,22 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Ammo: MonoBehaviour
+public class Ammo : MonoBehaviour
 {
-    public Image fillImage; // Reference to the fill image of the health bar
+    public Image barImage; // Reference to the Image component representing the empty bar
+    public Image fillImage; // Reference to the Image component representing the fill of the bar
+
     private float currentFill = 1f; // Current fill amount of the health bar, initially full
 
     void Start()
     {
         // Set the initial fill amount to full
         fillImage.fillAmount = 1f;
-    }
-
-    // Increase the fill amount of the health bar
-    public void IncreaseFill(float amount)
-    {
-        currentFill = Mathf.Clamp01(currentFill + amount);
-        fillImage.fillAmount = currentFill;
     }
 
     // Decrease the fill amount of the health bar
