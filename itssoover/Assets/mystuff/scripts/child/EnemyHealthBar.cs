b297a2a436db0a7a69ea51bec4740b0e7051b8ab
Sandeater88 <fw.ipowo@gmail.com>
2024-06-10 23:34:57 +0200

    huh

---
+++ itssoover/Assets/mystuff/scripts/child/EnemyHealthBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemmyHealthBar : MonoBehaviour
+{
+    public Image healthFill;
+
+    public Color fullColor = Color.green;
+    public Color midColor = Color.yellow;
+    public Color lowColor = Color.red;
+    public float midHealthThreshold = 0.5f;
+    public float lowHealthThreshold = 0.25f;
+
+    public void UpdateHealthBar(float healthPercentage)
+    {
+        healthFill.fillAmount = healthPercentage;
+
+        if (healthPercentage <= lowHealthThreshold)
+        {
+            healthFill.color = lowColor;
+        }
+        else if (healthPercentage <= midHealthThreshold)
+        {
+            healthFill.color = midColor;
+        }
+        else
+        {
+            healthFill.color = fullColor;
+        }
+    }
+}
